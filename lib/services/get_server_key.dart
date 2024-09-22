0892d8b3afb3c4d605a59a95eb4f74b2426ea2f8
Suleman Awan <73339313+sulemanawan1@users.noreply.github.com>
2024-09-22 17:38:27 +0300

    new Code

--- lib/services/get_server_key.dart
+++ lib/services/get_server_key.dart
@@ -4,9 +4,7 @@ class GetServerKey {
   GetServerKey._();
   static Future<String> getServerKeyToken() async {
     final scopes = [
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/firebase.messaging',
-      'https://www.googleapis.com/auth/firebase.database'
+      ''
     ];
 
     final client = await clientViaServiceAccount(
